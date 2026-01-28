aa55ffecaa43d0e3394b3c4d96aa1b09e8cc1258
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2026-01-28 17:33:15 +0100

    Remove the url

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,6 +1,4 @@
 const BASE_URL =
-  import.meta.env.MODE === "production"
-    ? "https://YOUR_RENDER_BACKEND_URL"
-    : "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export default BASE_URL;
