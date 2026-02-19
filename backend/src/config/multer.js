9c4b2701b6563255aeaa031d2a8b6f71a7198a7c
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2026-02-19 21:46:42 +0100

    Worked on the file size to 10mb

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -13,7 +13,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: { fileSize: 10 * 1024 * 1024 }, // 5MB
 });
 
 export default upload;
